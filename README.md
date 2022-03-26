981f5524e12d4f4444fd0c66a5913aa1bf2a54b7
Luan Teixeira <luanlts@outlook.com>
2022-03-26 19:11:11 -0300

    adicionado o README.md e terminado alguns detalhes no responsivo

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-"# api-rick-and-morty" 
+<h1 style="font-size: 3rem">
+    Rick and Morty - API
+</h1>
+<h2 style="font-size: 2.25rem">
+    Motivação
+</h2>
+<p style="text-align: justify;">O projeto da API do Rick and Morty surgiu após aprender sobre Axios para realizar requisições HTTP.</p>
+
+<h2 style="font-size: 2.25rem">
+    Aprendizado
+</h2>
+<p style="text-align: justify;">
+Foi possível aprender a realizar requisições HTTP com a biblioteca Axios e realizar manipulação dos dados que são retornados em objeto JSON.
+</p>
--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 const BASE_URL = `https://rickandmortyapi.com/api`;
 let json = document.getElementById("json");
+let oldCount = 1;
+let count = 11;
 
-const fetchPerson = async (count) => {
-    let { data } = await axios.get(`${BASE_URL}/character/${count}`);
-    console.log(data);
+const fetchPerson = async (oldCountP, countP) => {
+  if (countP >= 826 || oldCountP >= 826) return;
+  let cards = document.querySelector(".cards");
+  oldCount = oldCountP;
+  count = countP;
+  for (let i = oldCount; i < count; i++) {
+    if (i === 826) return;
+    console.log(i);
+    console.log(`${BASE_URL}/character/${i}`);
+    let { data } = await axios.get(`${BASE_URL}/character/${i}`);
     let { image, species, name, status } = data;
     let location = data.location.name;
     let origin = data.origin.name;
 
-    console.log(`NAME -> ${name}`);
-    console.log(`SPECIES -> ${species}`);
-    console.log(`IMAGE -> ${image}`);
-    console.log(`STATUS ->   ${status}`);
-    console.log(`LOCATION -> ${location}`);
-    console.log(`ORIGIN -> ${origin}`);
+    let card = `<div class="card">
+                    <div class="card-image">
+                    <img id="image" src="${image}" alt="">
+                </div>
+                <div class="card-infos">
+                    <div class="name">
+                    <span class="${status}" id="status">${status}</span>
+                        <p id="name">${name}</p>
+                    </div>
+                    <div class="location">
+                        <p id="location"><span id="location-title">Specie:</span> ${species}</p>
+                        <p id="location"><span id="location-title">Location:</span> ${location}</p>
+                        <p id="origin"><span style="font-weight: 600;color: #fff;">Origin:</span> ${origin}</p>
+                    </div>
+                </div>
+                </div>`;
+    cards.innerHTML += card;
+  }
 };
 
-fetchPerson(1);
+fetchPerson(oldCount, count);
